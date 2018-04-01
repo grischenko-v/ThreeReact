b227b48aadf79fd05d0db9b68a07a47877c76828
Vladimir <boooobka@gmail.com>
2018-04-01 19:25:07 +0400

    set diffrent colors for sides

--- src/Cube/Cube.js
+++ src/Cube/Cube.js
@@ -5,14 +5,23 @@ import * as THREE from 'three';
 class Cube extends Component {
     componentWillMount() {
         this.geometry = new THREE.BoxGeometry(1, 1, 1);
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFF000 });
+
+
+        for ( let i = 0; i < this.geometry.faces.length; i +=2 ) {
+            let color =  Math.random() * 0xffffff;
+            this.geometry.faces[ i ].color.setHex(color);
+            this.geometry.faces[ i + 1 ].color.setHex(color);
+        }
+
+
+        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
         this.cube = new THREE.Mesh(this.geometry, this.material);
 
         this.context.scene.add(this.cube);
 
-        var geo = new THREE.EdgesGeometry( this.cube.geometry ); // or WireframeGeometry
-        var mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 4 } );
-        var wireframe = new THREE.LineSegments( geo, mat );
+        let geo = new THREE.EdgesGeometry( this.cube.geometry ); // or WireframeGeometry
+        let mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 4 } );
+        let wireframe = new THREE.LineSegments( geo, mat );
         this.cube.add( wireframe );
     }
 
